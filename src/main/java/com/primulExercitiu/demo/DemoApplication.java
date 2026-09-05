96f9d5a9160d2a615550124b2282632205f0fd31
Eugen Scutaru <evgensc5@gmail.com>
2026-09-05 10:59:26 +0300

    Implement create feature

--- src/main/java/com/primulExercitiu/demo/DemoApplication.java
+++ src/main/java/com/primulExercitiu/demo/DemoApplication.java
@@ -17,7 +17,8 @@ public class DemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 
 	}
@@ -34,6 +35,19 @@ public class DemoApplication {
 
 		//afisam ID-ul studentului salvat
 		System.out.println("Saved student. Generated id:" + newStudent.getId());
+
+	}
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		System.out.println("Creating 3 student objects ...");
+		Student newStudent1 = new Student("Andrei", "Munteanu", "andrei@pixelacademy.md");
+		Student newStudent2 = new Student("Iulian", "Vataman", "iulic@pixelacademy.md");
+		Student newStudent3 = new Student("Maria", "Mirabela", "mira@pixelacademy.md");
+
+		//salvam obiectele student in baza de date
+		System.out.println("Saving the students ...");
+		studentDAO.save(newStudent1);
+		studentDAO.save(newStudent2);
+		studentDAO.save(newStudent3);
 
 	}
 
